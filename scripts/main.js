2962f62f00db7e4c7b42e1de0f51c73d38ef435b
BisquitDubouche <acapheles@gmail.com>
2023-02-01 14:34:31 +0600

    CRUD scripts

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,13 @@
-// add
+//! add inputs connection
 const addNameInp = document.querySelector("#add-name-inp");
 const addSurnameInp = document.querySelector("#add-surname-inp");
 const addPhoneNumberInp = document.querySelector("#add-phone-number-inp");
 const addWeekKpiInp = document.querySelector("#add-week-kpi-inp");
 const addMounthKpiInp = document.querySelector("#add-mounth-kpi-inp");
 const addStudentImgInp = document.querySelector("#add-img-inp");
-
 const studentsList = document.querySelector("#students-list");
 const addStudentBtn = document.querySelector("#add-student-btn");
 const saveStudentBtn = document.querySelector("#save-student-btn");
-// console.log(addNameInp,addSurnameInp, addPhoneNumberInp,addWeekKpiInp,addMounthKpiInp, studentsList);
-console.log(addStudentBtn);
 
 const STUDENTS_API = "http://localhost:8000/students";
 
@@ -72,6 +69,39 @@ async function render() {
               <p class="card-text">${i.mounth_KPI}</p>
               <a href="#" class="btn btn-primary edit-btn" data-bs-toggle="modal" data-bs-target="#staticBackdrop" id="edit-${i.id}">Edit</a>
               <a href="#" class="btn btn-primary delete-btn" id="del-${i.id}">Delete</a>
+
+
+
+
+              <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+              aria-labelledby="staticBackdropLabel" aria-hidden="true">
+              <div class="modal-dialog">
+                  <div class="modal-content">
+                      <div class="modal-header">
+                          <h5 class="modal-title" id="staticBackdropLabel"></h5>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Закрыть"></button>
+                      </div>
+                      <div class="modal-body">
+                          <div class="d-flex flex-column w-100 align-items-center" id="addStudent-block">
+                              <h2>Student information</h2>
+                              <input type="text" id="edit-name-inp" placeholder="Student name"
+                                  class="mb-2 w-100 form-control">
+                              <input type="text" id="edit-surname-inp" placeholder="Student surname"
+                                  class="mb-2 w-100 form-control">
+                              <input type="text" id="edit-phone-number-inp" placeholder="Phone number"
+                                  class="mb-2 w-100 form-control">
+                              <input type="text" id="edit-week-kpi-inp" placeholder="Week KPI"
+                                  class="mb-2 w-100 form-control">
+                              <input type="text" id="edit-mounth-kpi-inp" placeholder="Mounth KPI"
+                                  class="mb-2 w-100 form-control">
+      
+                              <input type="text" id="edit-img-inp" placeholder="Student photo Url"
+                                  class="mb-2 w-100 form-control">
+      
+                          </div>
+                          <div class="modal-footer  w-100 d-flex  justify-content-center">
+                              <button type="button" class="btn btn-outline-dark" id="save-student-btn">Save changes</button>
+                          </div>
           </div>
       </div>
       `;
@@ -79,6 +109,7 @@ async function render() {
 
   if (students.lenth === 0) return;
   addDeleteEvent();
+  addEditEvent();
 }
 render();
 //! render scripts end
@@ -97,4 +128,96 @@ function addDeleteEvent() {
   deleteStudentBtn.forEach((i) => i.addEventListener("click", deleteStudent));
 }
 //! delete student scripts end
+
+//! edit student scripts
+async function addStudentInfoToForm(e) {
+  let studentId = e.target.id.split("-")[1];
+
+  let req = await fetch(`${STUDENTS_API}/${studentId}`);
+  let studentObj = await req.json();
+
+  //! edit inputs connection
+  const editSurnameInp = document.querySelector("#edit-surname-inp");
+  const editNameInp = document.querySelector("#edit-name-inp");
+  const editPhoneNumberInp = document.querySelector("#edit-phone-number-inp");
+  const editWeekKpiInp = document.querySelector("#edit-week-kpi-inp");
+  const editMounthKpiInp = document.querySelector("#edit-mounth-kpi-inp");
+  const editStudentImgInp = document.querySelector("#edit-img-inp");
+
+//   console.log(
+//     editNameInp,
+//     editSurnameInp,
+//     editPhoneNumberInp,
+//     editWeekKpiInp,
+//     editMounthKpiInp,
+//     editStudentImgInp
+//   );
+
+  editStudentImgInp.value = "";
+  editNameInp.value = studentObj.name;
+  editSurnameInp.value = studentObj.surname;
+  editPhoneNumberInp.value = studentObj.phone_number;
+  editWeekKpiInp.value = studentObj.week_KPI;
+  editMounthKpiInp.value = studentObj.mounth_KPI;
+
+  let saveChangesBtn = document.querySelector("#save-student-btn");
+  saveChangesBtn.setAttribute("id", studentObj.id);
+
+
+  async function saveChanges(e) {
+    let updatedStudentObj = {
+      id: e.target.id,
+      photo: editStudentImgInp.value,
+      name: editNameInp.value,
+      surname: editSurnameInp.value,
+      phone_number: editPhoneNumberInp.value,
+      week_KPI: editWeekKpiInp.value,
+      mounth_KPI: editMounthKpiInp.value
+    };
+
+
+    if (
+        !addStudentImgInp.value.trim() ||
+        !addNameInp.value.trim() ||
+        !addSurnameInp.value.trim() ||
+        !addPhoneNumberInp.value.trim() ||
+        !addWeekKpiInp.value.trim() ||
+        !addMounthKpiInp.value.trim()
+      ) {
+        alert("Some inputs are empty");
+        return;
+      }
+
+
+    await fetch(`${STUDENTS_API}/${e.target.id}`, {
+      method: "PUT",
+      body: JSON.stringify(updatedStudentObj),
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+    });
+  
+    //! edit inputs clear
+    editPhoneNumberInp.value = "";
+    editNameInp.value = "";
+    editSurnameInp.value = "";
+    editPhoneNumberInp.value = "";
+    editWeekKpiInp.value = "";
+    editMounthKpiInp.value = "";
+
+    
+    saveChangesBtn.removeAttribute("id");
+    let btnClose = document.querySelector('.btn-close')
+    btnClose.click()
+    render();
+
+  }
+  saveChangesBtn.addEventListener("click", saveChanges);
+}
+
+function addEditEvent() {
+  let editBtn = document.querySelectorAll(".edit-btn");
+  editBtn.forEach((i) => i.addEventListener("click", addStudentInfoToForm));
+} 
+//! edit student scripts end
 
